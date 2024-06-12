cd6f6617d162e2eb31a43210616860416b58d360
Uriel Campaña <urielcamp@gmail.com>
2024-06-12 18:39:49 -0300

    push project

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,7 +1,34 @@
 public class Empleado {
 
     private String nombre;
-    private int Salario;
+    private int salario;
+
+    public Empleado(String nombre, int salario){
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public void calcularSalarioNeto(double impuesto){
+        salario *=  impuesto;
+    }
+
+    public void calcularSalarioNeto(double impuesto, double impuestoExtra){
+        salario *= (impuesto + impuestoExtra);
+    }
+
+    public void agregarBonificacion(double bonificacion){
+        salario += bonificacion;
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("informacion del empleado:");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Salario base: " + salario);
+    }
+
+
+
+
 
 
 
